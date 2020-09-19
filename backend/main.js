b181332d096f7b070b9d3dca65945bc5cbddfd26
Saurabh Totey <SaurabhTotey@gmail.com>
2020-09-19 15:56:30 -0600

    Basic project structure

---
+++ backend/main.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const app = express();
+
+// Serves frontend statically
+app.use(express.static("frontend"));
+
+// A dictionary of room id to the list of words
+const roomIdToWords = {};
+roomIdToWords[-1] = ["This", "is", "temporary", "testing", "data"]; //TODO: delete this line once we get everything working
+
+// When roomData is fetched, return a list of words for that room
+app.get("api/roomData", (req, res) => {
+	const roomId = req.query.roomId;
+	if (!(roomId in roomIdToWords)) {
+		res.send("TODO: the room id was not found")
+	}
+	res.json(roomIdToWords[roomId])
+});
+
+// Allow users to post words to a room
+// TODO: only allow one word at a time
+app.post("api/roomData", (req, res) => {
+	const word = req.query.word;
+	const roomId = req.query.roomId;
+	if (!(roomId in roomIdToWords)) {
+		res.send("TODO: the room id was not found")
+	}
+	roomIdToWords[roomId].push(word);
+	res.send(true);
+});
